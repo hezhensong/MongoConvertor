3eaf87fd14af64e7223fd10be07b20b5e90f492c
golphing <2645442899@qq.com>
2016-03-09 11:55:00 +0800

    pgc

--- mongodb/main.py
+++ mongodb/main.py
@@ -4,22 +4,26 @@
 from City import City
 from Weather import Weather
 from Activity import Activity
+from Pgc import Pgc
 
 
 def main():
-    address_old = 'localhost'
+    address_old = '123.56.65.17'
     port_old = 27017
-    address_new = '192.168.6.107'
+    address_new = '114.215.102.190'
     port_new = 27017
 
-    print("convert city data")
-    City.convert_city(address_old, port_old, address_new, port_new)
+    # print("convert city data")
+    # City.convert_city(address_old, port_old, address_new, port_new)
+    #
+    # print("convert weather data")
+    # Weather.convert_weather(address_old, port_old, address_new, port_new)
 
-    print("convert weather data")
-    Weather.convert_weather(address_old, port_old, address_new, port_new)
+    # print("convert activity data")
+    # Activity.convert_activity(address_old, port_old, address_new, port_new)
 
-    print("convert activity data")
-    Activity.convert_activity(address_old, port_old, address_new, port_new)
+    print("convert pgc data")
+    Pgc.convert_pgc(address_old,port_old,address_new,port_new)
 
 
 if __name__ == "__main__":
