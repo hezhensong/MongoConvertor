3cdce9af4ddc2fd5b358c6a0fa5ed5cf5f5e4e4b
golphing <2645442899@qq.com>
2016-03-09 10:05:17 +0800

    pgcs

---
+++ mongodb/Pgc.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+# from pymongo import ObjectId
+
+class Pgc:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def convert_pgc(address_old, port_old, address_new, port_new):
+        # old database connection
+        client = MongoClient(address_old, port_old)
+        travel1 = client.travel1
+
+        # new database connection
+        client = MongoClient(address_new, port_new)
+        travel2 = client.travel2
+        # old collection latest activities
+        pgcs = travel1.pgcs
+        pgcs_new = travel2.pgcs
+
+        # clean former data
+        pgcs_new.remove()
+
+        for pgc_old in pgcs.find():
+            # print(pgc_old)
+            _id = pgc_old['_id']
+            if 'title' in pgc_old:
+                title = pgc_old['title']
+            else:
+                cover_image = None
+
+            if 'cover_image' in pgc_old:
+                cover_image = pgc_old['cover_image']
+            else:
+                cover_image = None
+
+            if 'pgc_city' in pgc_old:
+               city_id=pgc_old['pgc_city']['_id']
+               # city_id1 = json.loads(city_id)
+               # print(city_id1._id)
+
+               print(unicode(city_id))
+
+
+            else:
+               pgc_city=None
+
+
+            post = {
+                '_id': _id,  # 活动ID
+                'city_id': ObjectId(city_id),  # 城市ID
+                'title': title,  # 活动主题
+                'cover_image': cover_image  # 活动封面
+            }
+            pgcs_new.insert(post)
