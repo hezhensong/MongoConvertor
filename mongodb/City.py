a796d8c218d713b6017a2e3b89bc2b140959ecd2
hezhensong <hezhensong@outlook.com>
2016-03-10 16:47:26 +0800

    为city添加 label list

--- mongodb/City.py
+++ mongodb/City.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 
 class City:
@@ -31,6 +32,17 @@ class City:
             city_name_en = city_old['cityname_en']
             city_name_py = city_old['cityname_py']
 
+            # 主题ID list
+            label_list = []
+            if 'attrlabels' in city_old:
+                label_list_old = city_old['attrlabels']
+                if len(label_list_old) > 0:
+                    for label_old in label_list_old:
+                        label = label_old['_id']
+                        if label != u'{{_id}}':
+                            label_list.append(ObjectId(label))
+
+            # 是否线上展示
             show_flag = city_old['show_flag']
             if show_flag == u'1':
                 is_show = True
@@ -42,6 +54,7 @@ class City:
                 'city_name': city_name,  # 城市中文名
                 'city_name_en': city_name_en,  # 城市英文名
                 'city_name_py': city_name_py,  # 城市中文名拼音
+                'label_list': label_list,  # 主题ID列表
                 'is_show': is_show  # 是否线上展示
             }
             city_new.insert(post)
